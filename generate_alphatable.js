7d6ff80dfbb1addcb41480042e0cf84f992523e2
bobbybee <j104@rocketmail.com>
2014-08-28 13:58:50 -0400

    Updated alphatable

--- generate_alphatable.js
+++ generate_alphatable.js
@@ -5,10 +5,13 @@ for(var i = 0; i < 255; ++i) {
 			(i >= "A".charCodeAt() && i <= "Z".charCodeAt())
 		||	(i >= "a".charCodeAt() && i <= "z".charCodeAt())
 		||  (i >= "0".charCodeAt() && i <= "9".charCodeAt())
+		||  (i >= 33 && i <= 47)
+		||  (i >= 58 && i <= 64)
+		||  (i >= 91 && i <= 126)
 	) {
 		o += String.fromCharCode(i);
 	} else {
-		o += "\\";
+		o += ".";
 	}
 }
 
