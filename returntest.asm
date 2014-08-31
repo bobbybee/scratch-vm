0806346fa9ad6b4b003474d3e7b76fcfa01c4e2b
bobbybee <j104@rocketmail.com>
2014-08-30 20:38:57 -0400

    Sample demonstrating return values

---
+++ returntest.asm
@@ -0,0 +1,24 @@
+; equilalent C code
+; void start() {
+; 	put(letter(26));
+; }
+;
+; void letter(int n) {
+;	return n + 64;
+; }
+
+start:
+PUSH A0
+PUSH A1
+PUSH #26
+CALL letter
+FRV A0
+OUT A0
+HLT
+
+letter:
+CAG A0, $FF
+LOAD A0, #64
+CAG A1, [sp+2]
+ADD A0, A1
+RET A0
